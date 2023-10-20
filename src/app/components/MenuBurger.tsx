665443597bfc6a4cc7798ce6c73f70a1a9a17288
BlazerXXI <103393085+BlazerXXI@users.noreply.github.com>
2023-10-20 08:42:02 +0300

    Update MenuBurger.tsx

--- src/app/components/MenuBurger.tsx
+++ src/app/components/MenuBurger.tsx
@@ -20,7 +20,7 @@ const MenuBurger = () => {
 				<span
 					className={`menu-burger__line w-[20px] h-[2px] bg-black rounded transition-all duration-300 ease-in-out after:w-[20px] after:h-[2px] after:bg-black after:rounded after:transition-all after:ease-in-out after:duration-300 after:content-[''] relative after:top-[6px] after:left-[0px] after:absolute before:w-[20px] before:h-[2px] before:bg-black before:rounded before:transition-all before:ease-in-out before:duration-300 before:content-[''] before:absolute before:bottom-[6px] before:left-[0px] ${
 						isOpen &&
-						"after:rotate-45 before:-rotate-45 after:top-[0px] before:bottom-[0px] bg-transparent"
+						"after:rotate-45 before:-rotate-45 after:bottom-[0px] before:top-[0px] bg-transparent"
 					}`}
 				></span>
 			</button>
