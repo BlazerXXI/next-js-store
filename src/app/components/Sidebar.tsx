8965381d33cb4a649b20fa67e9d5c05bbfddd499
BlazerXXI <103393085+BlazerXXI@users.noreply.github.com>
2023-10-21 08:25:37 +0300

    Update Sidebar.tsx

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -25,7 +25,6 @@ const Sidebar = (props: ISidebar) => {
 		<div
 			className={`menu-burger__menu translate-x-full bg-[rgb(255,255,255,0.9)] absolute right-0 top-0 md:w-[40vw] w-screen h-screen rounded z-[9] py-8 pt-16 px-5 shadow-lg ${propStyles} }`}
 		>
-			{/* TODO: add menu items */}
 			<ul className="flex flex-col gap-4 justify-center md:items-start items-center text-[20px] max-md:h-full">
 				<li>
 					<Link className="sidebar__link" rel="stylesheet" href="/">
