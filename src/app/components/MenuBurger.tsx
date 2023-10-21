ed20c3552cba2c89bb72389d049fcf40a8262e83
BlazerXXI <103393085+BlazerXXI@users.noreply.github.com>
2023-10-21 08:39:10 +0300

    Update MenuBurger.tsx

--- src/app/components/MenuBurger.tsx
+++ src/app/components/MenuBurger.tsx
@@ -2,45 +2,42 @@
 import React, { useEffect, useState } from "react";
 
 const MenuBurger = () => {
-	const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-useEffect(() => {
-	const openMenu = () => {
-		setIsOpen(!isOpen);
+  const openMenu = () => {
+    setIsOpen(!isOpen);
+    
+    const body = document.querySelector("body");
+    const menuBurgerLine = document.querySelector(".menu-burger__line");
 
-		const body = document.querySelector("body");
-		const menuBurgerLine = document.querySelector(".menu-burger__line");
+    if (body) {
+      body.classList.toggle("open-menu", isOpen);
+    }
 
-		if (body) {
-			body.classList.toggle("open-menu", isOpen);
-		}
+    if (menuBurgerLine) {
+      menuBurgerLine.classList.toggle("open-menu-burger", isOpen);
+    }
+  };
 
-		if (menuBurgerLine) {
-			menuBurgerLine.classList.toggle("open-menu-burger", isOpen);
-		}
-	};
-}, [])
+  const toggleMenu = () => {
+    openMenu();
+  }
 
-const toggleMenu = () => {
-openMenu()
-}
-
-	return (
-		<div className="menu-burger z-10">
-			{/* TODO: add menu burger (icons with mui of fw )  */}
-			{/* TODO: add cart, later functional for cart */}
-
-			<button
-				onClick={toggleMenu}
-				className="menu-burger__button flex justify-center items-center w-[40px] h-[40px] bg-none rounded cursor-pointer outline-none"
-			>
-				<span
-					className={`menu-burger__line w-[20px] h-[2px] bg-black rounded transition-all duration-300 ease-in-out after:w-[20px] after:h-[2px] after:bg-black after:rounded after:transition-all after:ease-in-out after:duration-300 after:content-[''] relative after:left-[0px] after:absolute before:w-[20px] before:h-[2px] before:bg-black before:rounded before:transition-all before:ease-in-out before:duration-300 before:content-[''] before:absolute before:top-[6px] after:bottom-[6px] before:left-[0px]
-					}`}
-				></span>
-			</button>
-		</div>
-	);
+  return (
+    <div className="menu-burger z-10">
+      {/* TODO: add menu burger (icons with mui of fw )  */}
+      {/* TODO: add cart, later functional for cart */}
+      
+      <button
+        onClick={toggleMenu}
+        className="menu-burger__button flex justify-center items-center w-[40px] h-[40px] bg-none rounded cursor-pointer outline-none"
+      >
+        <span
+          className={`menu-burger__line w-[20px] h-[2px] bg-black rounded transition-all duration-300 ease-in-out after:w-[20px] after:h-[2px] after:bg-black after:rounded after:transition-all after:ease-in-out after:duration-300 after:content-[''] relative after:left-[0px] after:absolute before:w-[20px] before:h-[2px] before:bg-black before:rounded before:transition-all before:ease-in-out before:duration-300 before:content-[''] before:absolute before:top-[6px] after:bottom-[6px] before:left-[0px]`}
+        ></span>
+      </button>
+    </div>
+  );
 };
 
 export default MenuBurger;
