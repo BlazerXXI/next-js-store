35a5a2295b3ef06bd303baa10d8d78d04f77bb20
BlazerXXI <ruslandendik@gmail.com>
2023-10-24 22:29:40 +0300

    fix

--- src/app/components/Popup.tsx
+++ src/app/components/Popup.tsx
@@ -13,8 +13,7 @@ const Popup = () => {
 			<div
 				className={`${
 					popup ? "flex" : "hidden"
-				} fixed top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] z-50 bg-white shadow-lg rounded p-4 md:first-letter:h-[400px] md:w-[400px] w-screen h-screen
-				mt-[60px]`}
+				} fixed top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] z-50 bg-white shadow-lg rounded p-4 md:first-letter:h-[400px] md:w-[400px] w-screen h-screen mt-[60px]`}
 			>
 				<h3>popup</h3>
 			</div>
